ac6850418c7323132ef2083f908f23f5adfd2839
Immanuella Busari <emi.busari@gmail.com>
2021-10-20 09:29:24 +0100

    Add dropdown options and buttons

--- MuskProcessServices/ListOfInspections.cs
+++ MuskProcessServices/ListOfInspections.cs
@@ -14,7 +14,66 @@ namespace MuskProcessServices
     {
         public ListOfInspections()
         {
+            // TODO: Rename variables accordingly.
             InitializeComponent();
+
+            // The code below is for testing purposes only
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDown;
+            comboBox1.Items.Add("Please select site");
+            comboBox1.Items.Add("Weetabix Burton");
+            comboBox1.Items.Add("Tyte and Lyle");
+            comboBox1.SelectedIndex = 0;
+            this.Controls.Add(comboBox1);
+        }
+
+        private void ListOfInspections_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
